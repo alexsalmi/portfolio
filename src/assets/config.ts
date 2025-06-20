85d4842102df13ba9919c378ec2db4c150c78c5b
alexsalmi <alexjfsalmi@gmail.com>
2025-06-20 15:55:51 -0400

    clean slate

--- src/assets/config.ts
+++
@@ -1,7 +0,0 @@
-// Environment variables with default values
-const env = {
-  CONTACT_REQUEST_API_URL: process.env.REACT_APP_CONTACT_REQUEST_API_URL ?? "http://localhost:3000/contact/send",
-  CONTACT_REQUEST_API_KEY: process.env.REACT_APP_CONTACT_REQUEST_API_KEY ?? "API_KEY"
-};
-
-export default env;
--- src/index.tsx
+++
@@ -1,5 +0,0 @@
-import ReactDOM from 'react-dom';
-import Router from './Router';
-import './styles/index.css';
-
-ReactDOM.render(<Router />, document.getElementById('root'));
