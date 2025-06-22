09f0477f6b3df66c015ce7f1febedaa3e1b9408c
alexsalmi <alexjfsalmi@gmail.com>
2025-06-22 17:05:51 -0400

    Add dropdown menu to header

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,3 @@
 export const THEME_TOGGLE_BUTTON_ID = "theme-toggle-button";
+export const MENU_TOGGLE_BUTTON_ID = "menu-toggle-button";
+export const DROPDOWN_MENU_ID = "dropdown-menu";
--- src/scripts/header.ts
+++ src/scripts/header.ts
@@ -1,10 +1,14 @@
+import { DROPDOWN_MENU_ID, MENU_TOGGLE_BUTTON_ID } from "../constants";
+
 export default (() => { 
   const header = document.querySelector("header");
+  const menuBtn = document.querySelector(`#${MENU_TOGGLE_BUTTON_ID}`);
+  const dropdownMenu = document.querySelector(`#${DROPDOWN_MENU_ID}`);
 
   let prevLoc = window.scrollY;
 
   document.addEventListener('scroll', () => {
-    if(!header)
+    if(!header || !dropdownMenu)
       return;
 
     const currDir = window.scrollY > prevLoc ? 'down' : 'up';
@@ -12,6 +16,7 @@ export default (() => {
     if(currDir === 'down' && window.scrollY > 200 && !header.classList.contains('animate-out')) {
       header.classList.add('animate-out');
       header.classList.remove('animate-in');
+      dropdownMenu.classList.remove("expanded");
     }
     else if(currDir === 'up' && header.classList.contains('animate-out')) {
       header.classList.remove('animate-out');
@@ -20,9 +25,24 @@ export default (() => {
 
     if(window.scrollY > 0 && !header.classList.contains('shadow'))
       header.classList.add('shadow');
-    else if (window.scrollY === 0 && header.classList.contains("shadow"))
+    else if (window.scrollY === 0 && !dropdownMenu.classList.contains("expanded") && header.classList.contains("shadow"))
       header.classList.remove('shadow');
 
     prevLoc = window.scrollY;
+  });
+
+  menuBtn?.addEventListener('click', () => {
+    if(!header || !dropdownMenu)
+      return;
+
+    if(dropdownMenu.classList.contains("expanded")){
+      dropdownMenu.classList.remove("expanded");
+      if(window.scrollY === 0)
+        header.classList.remove('shadow');
+    }
+    else {
+      header.classList.add('shadow');
+      dropdownMenu.classList.add("expanded");
+    }
   });
 })();
