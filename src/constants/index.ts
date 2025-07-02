abfb2e602215ac874082bae792a86cd15a9e08d2
Alex Salmi <alexjfsalmi@gmail.com>
2025-07-02 15:18:43 -0400

    Code cleanup

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,4 @@ export * from "./ids";
 export * from "./socials";
 export * from "./skills";
 export * from "./experience";
+export * from "./projects";
--- src/scripts/animation.ts
+++ src/scripts/animation.ts
@@ -1,7 +1,9 @@
 import { SCROLL_ANIMATION_ID } from "../constants";
 
 export default (() => {
+  // Create an observer to check if elements are in the window view
   const observer = new IntersectionObserver((entries) => {
+    // For every observed element in the window view, give them the animate-in class
     for (const entry of entries) {
       if (entry.isIntersecting) {
         entry.target.classList.add("animate-in");
@@ -10,8 +12,8 @@ export default (() => {
     }
   });
 
+  // Add all elements with the scroll animation id to the observer
   const elements = document.querySelectorAll(`#${SCROLL_ANIMATION_ID}`);
-
   for (const element of elements) {
     observer.observe(element);
     element.classList.add("pre-animation");
